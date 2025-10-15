242003b14af0dc88748f041e53f6d3bd30410761
taysirus <61semh61@gmail.com>
2025-10-16 02:14:37 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+app/page.jsx
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '100px 20px' }}>
+      <h1 style={{ fontSize: '40px', fontWeight: 'bold' }}>Semih Tech Bilgisayar</h1>
+      <p style={{ marginTop: '10px', color: '#bbb' }}>Yeni, ikinci el ve özel toplama bilgisayarlar.</p>
+      <p style={{ marginTop: '30px' }}>Site başarıyla çalışıyor ✅</p>
+    </div>
+  );
+}
